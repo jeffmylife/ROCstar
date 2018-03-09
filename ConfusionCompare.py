cdfc8362226c5b1f1e51a1d7ee01538c9ca2928e
jeffmylife <jeffmlife@gmail.com>
2018-03-08 18:12:44 -0600

    Creation of Classes
    Creation of all classes involved in the ROCstar module.

---
+++ ConfusionCompare.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#This is the ConfusionCompare class. 
+# Stories related to this class include : 
+#1) F4 : Comprehensive overview of classifier(s) performance
+
+
---
+++ ROCstar.py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# This class is the ROCstar class. 
+# Stories related to this class include : 
+#1) F3 : Dynamic handling of input data types 
+
