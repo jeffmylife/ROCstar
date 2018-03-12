aca03ce41e277fd7881a58857fe55aca98196a12
Unknown <elyk33366@gmail.com>
2018-03-11 21:42:12 -0500

    Created Class and PercentDifference Function

--- ConfusionCompare.py
+++ ConfusionCompare.py
@@ -4,7 +4,11 @@
 # In[ ]:
 
 
-#This is the ConfusionCompare class.  
-# Stories related to this class include :  
-#1) F4 : Comprehensive overview of classifier(s) performance 
+#This is the ConfusionCompare class.
+# Stories related to this class include :
+#1) F4 : Comprehensive overview of classifier(s) performance
+class ConfusionCompare:
 
+    def percentDifference(auc1, auc2):
+        """Returns the percent difference between two AUC's"""
+        return abs(auc1-auc2) / (auc1+auc2) * 2
