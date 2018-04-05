2fc9f913b0a1c4837b7204f0553221bb20430451
Jeffrey Lemoine <jeffmlife@gmail.com>
2018-04-05 10:37:21 -0500

    Incorporate ConfusionVisual and allow for multiple or many confusion matrices
    Fixex the .apply function to be able to handle one or many confusion matrices

--- ROCstar.py
+++ ROCstar.py
@@ -1,5 +1,6 @@
 import ConfusionStatistics 
 import ConfusionMatrix
+import ConfusionVisual
 import numpy  
 from sklearn.preprocessing import label_binarize
 
@@ -22,6 +23,7 @@ def binarize(lst):
 class ROCstar:
     STATS = removePrivateMethods(dict(ConfusionStatistics.stat.__dict__))
     MATRIX = removePrivateMethods(dict(ConfusionMatrix.ConfusionMatrix.__dict__))
+    VISUAL = removePrivateMethods(dict(ConfusionVisual.ConfusionVisual.__dict__))
     
     def __init__(self,actual = [], preds: dict ={}):
         self.setActual(actual)
@@ -34,8 +36,11 @@ class ROCstar:
         '''Updates self.CONFS to new ACTUAL or PREDS values'''
         for model in self.PREDS:
             assert len(self.ACTUAL)==len(self.PREDS[model]),"Predicted and actual data must be of equal length"
-            self.CONFS.update( {model: ConfusionMatrix.ConfusionMatrix(actual=self.ACTUAL,pred=self.PREDS[model])} )
-        
+            if len(set(self.PREDS[model]))>2:
+                self.CONFS.update( {model: ConfusionMatrix.ConfusionMatrix(actual=self.ACTUAL,pred=self.PREDS[model],bins=10).getConfs()} )
+            else:
+                self.CONFS.update( {model: ConfusionMatrix.ConfusionMatrix(actual=self.ACTUAL,pred=self.PREDS[model])} )
+                
     def setActual(self,actual):
         self.ACTUAL = binarize(actual)  
         try:
@@ -61,12 +66,17 @@ class ROCstar:
         
     
     def apply(self,function: str) -> dict:
-        return {model:self.STATS[function](*self.CONFS[model]) for model in self.CONFS}
+        if list(self.CONFS.items())[0][1].__class__==dict:
+            return { model: [self.STATS[function](*self.CONFS[model][thresh]) for thresh in self.CONFS[model]] for model in self.CONFS }
+        else:
+            return {model : self.STATS[function](*self.CONFS[model]) for model in self.CONFS}
+            
+        
     
     def plot(self,x_axis: str ="FPR",y_axis: str ="TPR"):
         x_data = self.apply(x_axis)
         y_data = self.apply(y_axis)
-        vis = ConfusionVisual()
+        vis = ConfusionVisual.ConfusionVisual()
         for model in self.CONFS:
             vis.add(x=x_data[model],x_title=x_axis,y=y_data[model],y_title=y_axis,title=model)
     
@@ -74,8 +84,7 @@ class ROCstar:
         plt.show()
         
     
-    
-'''
+'''    
 r = ROCstar((1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1),{"pred1":(1,0.6,1.0,.6,.7,1,1,0,1,1,0,1,.5,0,.1,1,0.7,1,.1,0,1,1,0,1)})
 r.plot()
-'''
+''' 
