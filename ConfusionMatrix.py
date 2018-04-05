79456b1ae00a7bf701db0096e8134f2e32208121
Jeffrey Lemoine <jeffmlife@gmail.com>
2018-04-04 20:05:40 -0500

    Update ConfusionMatrix
    Now can hold one or many confusion matrices and uses more common data structures

--- ConfusionMatrix.py
+++ ConfusionMatrix.py
@@ -1,67 +1,56 @@
 
-# coding: utf-8
+from numpy import nan,arange 
 
-# In[ ]:
-
-
-from numpy import nan 
- 
 class ConfusionMatrix: 
-     
-    def __init__(self,pred=[],actual=[],tp=0,fp=0,tn=0,fn=0): 
-        if len(pred)!=0: 
-            pred_vals,actual_vals = set(pred), set(actual) 
-             
-            if len(pred_vals)!=2: 
-                raise ValueError("Non-Binary Values") 
- 
-            c1,c2 = sorted(list(pred_vals))     #classifications like 0 or 1, cat or dog, etc.  
-             
-            for prd,act in zip(pred,actual):    #note that pred and actual do not have to be equal length 
-                if prd==act:                    #predicted correctly 
-                    if prd==c2:                  
-                        tp+=1               
-                    else:                        
-                        tn+=1 
-                else: 
-                    if prd==c2:                  
-                        fp+=1               
-                    else:                        
-                        fn+=1  
-        else: 
-            if (tp+tn+fn+fp).__class__!=int: 
-                raise ValueError("") 
-         
-        self.tp = tp 
-        self.fp = fp 
-        self.tn = tn 
-        self.fn = fn 
-     
- 
-    def __iter__(self):         
-        return (x for x in [self.tp,self.fp,self.tn,self.fn]) 
-         
-         
-    #also could use (x > 0.5) where x is numpy array  
-    def fromThresh(pred,actual,thresh): 
+    
+    def calcConfusion(pred,actual,threshold=.5): 
         actual_vals = set(actual) 
         if len(actual_vals)!=2: 
-            raise ValueError("Non-Binary Values") 
-         
-        true_val=sorted(list(actual_vals))[1] 
-         
+            raise ValueError("non-binary values not allowed ") 
+                  
         tp=fp=tn=fn=0 
-        for aval,pval in zip(actual,pred): 
-                if aval==true_val:  #true 
-                    if pval>thresh: #positive 
+        for actu_val,pred_val in zip(actual,pred): 
+                if actu_val==1:      
+                    if pred_val>threshold: 
                         tp+=1 
                     else: 
                         fn+=1 
-                else:               #false 
-                    if pval<thresh:  
+                else:                
+                    if pred_val<threshold:  
                         tn+=1 
                     else:  
                         fp+=1 
         return ConfusionMatrix(tp=tp,fp=fp,tn=tn,fn=fn) 
- 
-
+     
+    def __init__(self,pred=[],actual=[],tp=0,fp=0,tn=0,fn=0,bins=100): 
+        assert len(pred)==len(actual)
+        
+        self.tp = tp 
+        self.fp = fp
+        self.tn = tn 
+        self.fn = fn 
+        self.confs = None
+        
+        '''Calculate all confusion matrices'''
+        if len(pred)!=0 : 
+            low = min(pred)
+            high = max(pred)
+            step = (low+high)/bins
+            
+            self.tp = self.fp = self.tn = self.fn = None
+            confs = {}
+            for thresh in arange(high,low-step,-step):
+                conf = ConfusionMatrix.calcConfusion(pred,actual,threshold=thresh) 
+                confs.update({thresh:conf})
+            self.confs=confs
+            
+    
+    def getConfs(self):
+        confs={}
+        for thresh in self.confs:
+            confs[thresh]= tuple([i for i in self.confs[thresh]])
+        return confs
+    
+    def __iter__(self):         
+        return (x for x in [self.tp,self.fp,self.tn,self.fn]) 
+         
